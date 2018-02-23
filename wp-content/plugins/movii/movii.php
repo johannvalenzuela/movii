59da568bc148a598ad1806d917c4be1b45ec4092
Johann <johann.aevt@gmail.com>
2018-02-23 13:11:56 -0300

    feat: plugin description

---
+++ wp-content/plugins/movii/movii.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Movii Plugin
+ * Author: Johann Valenzuela
+ * Author URI: http://inteligeek.cl
+ * Version: 0.1
+ * Description: Este plugin permite obtener una lista de puntos de georeferencia, mostrarlos en una lista y en un mapa. Todo utilizando Google Maps API.
+ */
